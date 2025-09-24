560fa0c99685468fbc672a0ad7fa14326a2872bd
Karl-Andreas Meus <karl.andreas@meus.ee>
2025-09-24 18:19:12 +0300

    Remove text

--- mobile/components/AddFriendModal.tsx
+++ mobile/components/AddFriendModal.tsx
@@ -10,5 +10,5 @@ export default function AddFriendModal({
   onClose: () => void;
   onAddFriend: (friendData: { name: string; contactFrequency: number }) => void;
 }) {
-  return <Text>AddFriendModal</Text>;
+  return <></>;
 }
